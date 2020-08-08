3f966268d12d1312a816694a8df7fc3b1a81c5f8
ArbabMemon <64311080+ArbabMemon@users.noreply.github.com>
2020-08-08 12:56:53 +0500

    Update update.sh

--- core/update/update.sh
+++ core/update/update.sh
@@ -35,7 +35,7 @@ echo -e "$grn                updating the HackLock please wait$rset"
 sleep 2.0
 cd $HOME
 rm -rf hacklock
-git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/ArbabMemon/hacklock-1
 clear
 echo " "
 echo -e "$grn               HackLock has been updated check it now...$rset"
